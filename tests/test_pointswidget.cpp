f71959011f97afc9d4941acb50ddaddb6df6809f
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2026-04-10 21:06:43 +0300

    test: добавил unit-тесты для класса PointsWidget

---
+++ tests/test_pointswidget.cpp
@@ -0,0 +1,95 @@
+#include <QtTest/QtTest>
+
+#include <QColor>
+#include <QImage>
+#include <QPalette>
+
+#include "pointswidget.h"
+#include "mypoint.h"
+
+class TestPointsWidget : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void constructor_initialState_isEmpty();
+    void addPoint_storesPointAndIncreasesCount();
+    void clearPoints_removesAllPoints();
+    void clearPoints_whenAlreadyEmpty_remainsEmpty();
+    void render_afterAddingPoint_drawsExpectedColor();
+};
+
+void TestPointsWidget::constructor_initialState_isEmpty()
+{
+    PointsWidget widget;
+
+    QCOMPARE(widget.pointCount(), 0);
+    QVERIFY(widget.points().isEmpty());
+}
+
+void TestPointsWidget::addPoint_storesPointAndIncreasesCount()
+{
+    PointsWidget widget;
+
+    const MyPoint first(QPoint(3, 10), Qt::red);
+    const MyPoint second(QPoint(7, 20), Qt::blue);
+
+    widget.addPoint(first);
+    widget.addPoint(second);
+
+    QCOMPARE(widget.pointCount(), 2);
+    QCOMPARE(widget.points().size(), 2);
+    QCOMPARE(widget.points().at(0), first);
+    QCOMPARE(widget.points().at(1), second);
+}
+
+void TestPointsWidget::clearPoints_removesAllPoints()
+{
+    PointsWidget widget;
+
+    widget.addPoint(MyPoint(QPoint(1, 5), Qt::red));
+    widget.addPoint(MyPoint(QPoint(2, 6), Qt::green));
+    QCOMPARE(widget.pointCount(), 2);
+
+    widget.clearPoints();
+
+    QCOMPARE(widget.pointCount(), 0);
+    QVERIFY(widget.points().isEmpty());
+}
+
+void TestPointsWidget::clearPoints_whenAlreadyEmpty_remainsEmpty()
+{
+    PointsWidget widget;
+
+    widget.clearPoints();
+
+    QCOMPARE(widget.pointCount(), 0);
+    QVERIFY(widget.points().isEmpty());
+}
+
+void TestPointsWidget::render_afterAddingPoint_drawsExpectedColor()
+{
+    PointsWidget widget;
+    widget.resize(20, 20);
+
+    QPalette palette = widget.palette();
+    palette.setBrush(QPalette::Base, QBrush(Qt::white));
+    widget.setPalette(palette);
+
+    widget.addPoint(MyPoint(QPoint(5, 7), Qt::red));
+
+    QImage image(widget.size(), QImage::Format_ARGB32);
+    image.fill(Qt::black);
+
+    widget.render(&image);
+
+    for (int y = 7; y < image.height(); ++y) {
+        QCOMPARE(image.pixelColor(5, y), QColor(Qt::red));
+    }
+
+    QCOMPARE(image.pixelColor(4, 7), QColor(Qt::white));
+    QCOMPARE(image.pixelColor(6, 7), QColor(Qt::white));
+}
+
+QTEST_MAIN(TestPointsWidget)
+#include "test_pointswidget.moc"
