c9632edba7c2368a132600ed6cfad92bb64f3e47
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2026-04-10 21:25:09 +0300

    refactor: привел MainWindow к аккуратному базовому каркасу

--- app/include/mainwindow.h
+++ app/include/mainwindow.h
@@ -9,16 +9,63 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+/**
+ * @brief Главное окно первого приложения лабораторной работы по потокам.
+ *
+ * @details
+ * На текущем этапе класс отвечает за:
+ * - инициализацию пользовательского интерфейса;
+ * - подключение QAction из меню и toolbar;
+ * - базовые слоты-команды приложения.
+ *
+ * Логика отображения точек и запуска рабочих объектов будет добавлена
+ * на следующих шагах.
+ */
+class MainWindow final : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Создаёт главное окно приложения.
+     * @param parent Родительский виджет.
+     */
     explicit MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Освобождает ресурсы главного окна.
+     */
     ~MainWindow() override;
 
+private slots:
+    /**
+     * @brief Заглушка под будущий запуск worker'ов через QtConcurrent.
+     */
+    void slotQtConcurrent();
+
+    /**
+     * @brief Заглушка под будущий запуск worker'ов через QRunnable.
+     */
+    void slotQRunnable();
+
+    /**
+     * @brief Заглушка под будущую очистку области отображения.
+     */
+    void slotClear();
+
 private:
-    Ui::MainWindow *ui = nullptr;
+    /**
+     * @brief Выполняет базовую настройку UI.
+     */
+    void initializeUi();
+
+    /**
+     * @brief Подключает QAction из .ui к слотам MainWindow.
+     */
+    void connectActions();
+
+private:
+    Ui::MainWindow *ui = nullptr; ///< Сгенерированный интерфейс формы.
 };
 
 #endif // MAINWINDOW_H
--- app/src/mainwindow.cpp
+++ app/src/mainwindow.cpp
@@ -1,15 +1,68 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QAction>
+#include <QStatusBar>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("Потоки + worker");
+
+    initializeUi();
+    connectActions();
 }
+
+//----------------------------------------------------------------------------
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//----------------------------------------------------------------------------
+
+void MainWindow::slotQtConcurrent()
+{
+    statusBar()->showMessage(
+        QStringLiteral("Запуск через QtConcurrent будет добавлен следующим шагом."));
+}
+
+//----------------------------------------------------------------------------
+
+void MainWindow::slotQRunnable()
+{
+    statusBar()->showMessage(
+        QStringLiteral("Запуск через QRunnable будет добавлен следующим шагом."));
+}
+
+//----------------------------------------------------------------------------
+
+void MainWindow::slotClear()
+{
+    statusBar()->showMessage(
+        QStringLiteral("Очистка области отображения будет добавлена следующим шагом."));
+}
+
+//----------------------------------------------------------------------------
+
+void MainWindow::initializeUi()
+{
+    setWindowTitle(QStringLiteral("Потоки + worker"));
+    statusBar()->showMessage(QStringLiteral("Приложение готово к работе"));
+}
+
+//----------------------------------------------------------------------------
+
+void MainWindow::connectActions()
+{
+    connect(ui->actionQtConcurrent, &QAction::triggered,
+            this, &MainWindow::slotQtConcurrent);
+
+    connect(ui->actionQRunnable, &QAction::triggered,
+            this, &MainWindow::slotQRunnable);
+
+    connect(ui->actionClear, &QAction::triggered,
+            this, &MainWindow::slotClear);
 }
