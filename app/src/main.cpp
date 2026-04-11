d17dddb4a8dbab883e7aadb036852a7a94f667c7
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2026-04-11 12:34:24 +0300

    feat: добавил привязку процесса к одному ядру в main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,8 +2,16 @@
 
 #include <QApplication>
 
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_WIN
+    SetProcessAffinityMask(GetCurrentProcess(), 1);
+#endif
+
     QApplication app(argc, argv);
 
     MainWindow window;
